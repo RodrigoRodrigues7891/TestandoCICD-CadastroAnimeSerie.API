13712fb9847408ce7af5a9b1a8d6b6a37332a032
Rodrigo Rodrigues de Abreu <rodrigorodrigues7891@gmail.com>
2025-04-01 17:19:13 -0300

    Adicionar arquivos de projeto.

---
+++ test/CadastroAnimeSerie.API.Test/CadastroAnimeSerieWebApplicationFactory.cs
@@ -0,0 +1,17 @@
+﻿using CadastroAnimeSerie.Dados.Banco;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CadastroAnimeSerie.API.Test;
+
+public class CadastroAnimeSerieWebApplicationFactory: WebApplicationFactory<Program>
+{
+    public CadastroAnimeSerieContext Context { get; }
+    private IServiceScope scope;
+
+    public CadastroAnimeSerieWebApplicationFactory()
+    {
+        this.scope = Services.CreateScope();
+        Context = scope.ServiceProvider.GetRequiredService<CadastroAnimeSerieContext>();
+    }
+}
